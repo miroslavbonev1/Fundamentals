80dc4067285ec8ab95fdd8825bcd08d376b8203f
miroslavbonev1 <146978073+miroslavbonev1@users.noreply.github.com>
2023-10-04 20:23:09 +0300

    new

---
+++ src/Methods/P02Grade.java
@@ -0,0 +1,24 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class P02Grade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = Double.parseDouble(scanner.nextLine());
+        printGradeResult(grade);
+    }
+    public static void printGradeResult(double grade){
+        if (grade >= 2 && grade <= 2.99){
+            System.out.println("Fail");
+        } else if (grade >= 3 && grade <= 3.49) {
+            System.out.println("Poor");
+        }else if (grade >= 3.5 && grade <= 4.49) {
+            System.out.println("Good");
+        }else if (grade >= 4.5 && grade <= 5.49) {
+            System.out.println("Very good");
+        } else if (grade>= 5.5 && grade <= 6.00) {
+            System.out.println("Excellent");
+        }
+    }
+}
